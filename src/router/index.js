c60544093f9423ef202e9de87b252a5196150e57
josehb96 <josehurtadobaeza@gmail.com>
2024-09-07 22:47:48 +0200

    Vue Router integration in our Vue 3 project

---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter, createMemoryHistory } from 'vue-router'
+
+const routes = []
+
+const router = createRouter({
+    history: createMemoryHistory(), // Para crear el historial de navegación en memoria
+    routes
+})
