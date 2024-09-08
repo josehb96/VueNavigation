487df32792a64954c944222276f23e11005a73b3
josehb96 <josehurtadobaeza@gmail.com>
2024-09-08 02:29:09 +0200

    Declare the first routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,28 @@
 import { createRouter, createMemoryHistory } from 'vue-router'
 
-const routes = []
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import BlogView from '../views/BlogView.vue'
+
+const routes = [
+   {
+       path: '/',
+       component: HomeView
+   },
+   {
+       path: '/about',
+       component: AboutView 
+   },
+   {
+       path: '/blog',
+       component: BlogView
+   } 
+
+]
 
 const router = createRouter({
     history: createMemoryHistory(), // Para crear el historial de navegación en memoria
     routes
 })
+
+export default router
