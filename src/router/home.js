289e9809bad24de821529b9f766f8bd1ad12a568
josehb96 <josehurtadobaeza@gmail.com>
2024-09-14 19:17:48 +0200

    404 Routes - Page Not Found

--- src/router/home.js
+++ src/router/home.js
@@ -3,38 +3,43 @@ import AboutView from '../views/AboutView.vue'
 import BlogView from '../views/BlogView.vue'
 import EntradaBlogView from '../views/EntradaBlogView.vue'
 import SearchBlog from '../views/SearchBlogView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 export const homeRoutes = [
-   {
-       path: '/',
-       component: HomeView,
-       name: 'home'
-   },
-   {
-       path: '/about',
-       component: AboutView,
-       name: 'about' 
-   },
-   {
-       path: '/entra/:id(//d+)', // Forzamos a que sólo aceptemos valores numéricos para dicho parámetro
-       component: EntradaBlogView,
-       name: 'blog-entrada'
-   }, 
-   {
-       path: '/search-blog',
-       component: SearchBlog,
-       name: 'search-blog'
-   },
-   {
-       path: '/blog',
-       component: BlogView,
-       name: 'blog',
-       children: [
-           {
-               path: 'detail/:id',
-               component: EntradaBlogView,
-               name: 'blog-detail'
-           }
-       ]
-   }
+    {
+        path: '/:pathMatch(.*)*', // Usamos una expresión regular que sirva para detectar cualquier otro texto que no esté en las rutas existentes
+        component: NotFoundView
+    },
+    {
+        path: '/',
+        component: HomeView,
+        name: 'home'
+    },
+    {
+        path: '/about',
+        component: AboutView,
+        name: 'about' 
+    },
+    {
+        path: '/entra/:id(//d+)', // Forzamos a que sólo aceptemos valores numéricos para dicho parámetro
+        component: EntradaBlogView,
+        name: 'blog-entrada'
+    }, 
+    {
+        path: '/search-blog',
+        component: SearchBlog,
+        name: 'search-blog'
+    },
+    {
+        path: '/blog',
+        component: BlogView,
+        name: 'blog',
+        children: [
+            {
+                path: 'detail/:id',
+                component: EntradaBlogView,
+                name: 'blog-detail'
+            }
+        ]
+    }
 ]
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h3>No pudimos encontrar la página :(</h3>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
